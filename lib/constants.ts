6894541c53aa6869e5438989360a1d0bf95c5383
Andrew Truex <andrewtruex@gmail.com>
2025-10-01 02:37:07 -0400

    initial commit

---
+++ lib/constants.ts
@@ -0,0 +1,9 @@
+export const TABLES = {
+  EMAIL_LIST: "email-list",
+}
+
+export const socialLinks = {
+  website: "https://andrewtruex.com",
+  x: "https://x.com/web___guy",
+  github: "https://github.com/ajtruex",
+}
